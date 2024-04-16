65083fb9f9c4c438e12801440ea0422967ce6fa8
Tetiana Huseva <gusievatania2004@gmail.com>
2024-04-16 16:33:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function makeCalculator() {
+  const calculator = {
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    divide(value) {
+      this.result /= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    result: 0,
+
+    operate(callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+  };
+
+  return calculator;
+}
